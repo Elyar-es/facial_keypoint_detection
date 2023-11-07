c41ffef3c4b05bd08a9b5e5b76fbde0325ae3ef4
Elyar <111648569+Elyar-es@users.noreply.github.com>
2023-11-07 19:21:09 +0330

    Update renamer.py

--- Code/renamer.py
+++ Code/renamer.py
@@ -3,12 +3,14 @@ import sys
 
 def change_character_in_image_names(folder, index, character):   # Changes the character at the specified index in the names of the images in the specified folder to the specified character.
 
-
-  for image in os.listdir(folder):
-    if image.endswith(".jpg"):
-      current_name = os.path.basename(image)
-      new_name = current_name[:index] + character + current_name[index + 1:]
-      os.rename(os.path.join(folder, image), os.path.join(folder, new_name))
+    for image in os.listdir(folder):
+      
+        if image.endswith(".jpg"):
+          
+          current_name = os.path.basename(image)
+          
+            new_name = current_name[:index] + character + current_name[index + 1:]
+            os.rename(os.path.join(folder, image), os.path.join(folder, new_name))
 
 if __name__ == "__main__":
   folder = sys.argv[3]
